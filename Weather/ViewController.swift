55f91a372a9a495a91890669c47fdc08314ccce1
Maxim <maksismart200@gmail.com>
2022-09-13 16:34:04 +0300

    Create a project

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Максим Кузнецов on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
